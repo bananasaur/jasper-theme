2b149b757425850771577b435b435b0191503046
Eric Lea <eric.leaa@gmail.com>
2015-07-21 14:42:03 +0800

    Jasper theme init from Casper With gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var path = require('path');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var jshint = require('gulp-jshint');
+var watch = require('gulp-watch');
+var changed = require('gulp-changed');
+var gutil = require('gulp-util');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('lint', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    // .pipe(jshint.reporter('fail'))
+    .on('error', gutil.log)
+    ;
+});
+
+gulp.task('less', function() {
+  // need to minify css
+  // need to use autoprefix
+  return gulp.src('src/less/style-custom.less')
+    .pipe(changed('assets/css'))
+    .pipe(less())
+    .pipe(gulp.dest('assets/css'))
+    .on('error', gutil.log)
+    ;
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('assets/js'))
+    .pipe(rename('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'))
+    .on('error', gutil.log)
+    ;
+});
+
+gulp.task('watch', function() {
+  // do i really want to concat every js file into one scripts file?
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/less/**/*.less', ['less']);
+});
+
+gulp.task('default', function() {
+  gulp.start('lint', 'scripts', 'less', 'watch')
+  ;
+});
